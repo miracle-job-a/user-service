f604a1c0d5ac10f864452922747bd5b622e9f9f8
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-11-15 21:04:23 +0900

    Feat : 토큰 검사 필터 추가

---
+++ src/main/java/com/miracle/userservice/controller/error/ErrorController.java
@@ -0,0 +1,18 @@
+package com.miracle.userservice.controller.error;
+
+import com.sun.jdi.request.InvalidRequestStateException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/error")
+@RestController
+public class ErrorController {
+
+    @GetMapping("/invalid-request")
+    public void invalidRequest() {
+        throw new InvalidRequestStateException("올바르지 않은 요청입니다.");
+    }
+}
