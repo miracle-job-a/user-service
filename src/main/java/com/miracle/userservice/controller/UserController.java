53fd55f66d6986a1f588375b56abf6c40dfb3adf
hazzokko <hazzokko@gmail.com>
2023-11-20 16:31:10 +0900

    Fix : 로그인 로직 수정(UserController)
    1. @ResponseStatus 어노테이션 추가
    2. HttpServletResponse를 추가하여 로그인 실패 시, 요청은 성공으로 예외를 발생시키진 않으나, HttpStatus를 400으로 반환하는 것으로 변경(기존 커스텀 예외들 모두 삭제)

--- src/main/java/com/miracle/userservice/controller/UserController.java
+++ src/main/java/com/miracle/userservice/controller/UserController.java
@@ -1,19 +1,17 @@
 package com.miracle.userservice.controller;
 
-import com.miracle.userservice.dto.response.ApiResponse;
-import com.miracle.userservice.dto.response.SuccessApiResponse;
 import com.miracle.userservice.dto.request.UserLoginRequestDto;
-import com.miracle.userservice.entity.User;
+import com.miracle.userservice.dto.response.CommonApiResponse;
+import com.miracle.userservice.dto.response.SuccessApiResponse;
 import com.miracle.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,12 +21,29 @@ public class UserController {
 
     private final UserService userService;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
-    public ApiResponse login(@Valid @RequestBody UserLoginRequestDto dto, HttpServletRequest request) {
-        log.debug("dto = {}", dto);
-        userService.login(dto);
+    public CommonApiResponse login(@Valid @RequestBody UserLoginRequestDto dto, HttpServletRequest request, HttpServletResponse response) {
+        String sessionId = request.getHeader("sessionId");
+        log.debug("sessionId = {}, dto = {}", sessionId, dto);
 
-        String message = "로그인에 성공했습니다.";
-        return new SuccessApiResponse<>(HttpStatus.OK.value(), message, null);
+        boolean login = userService.login(dto);
+
+        String message;
+        int httpStatus;
+        Boolean data;
+
+        if(login) {
+            message = "로그인에 성공했습니다.";
+            httpStatus = HttpStatus.OK.value();
+            data = null;
+            response.setStatus(httpStatus);
+        } else {
+            message = "로그인에 실패했습니다.";
+            httpStatus = HttpStatus.BAD_REQUEST.value();
+            data = false;
+            response.setStatus(httpStatus);
+        }
+        return new SuccessApiResponse<>(httpStatus, message, data);
     }
 }
