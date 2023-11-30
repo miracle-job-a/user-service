f65935dd9f5562256e2f4c441db8a5edd6a1f352
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2023-12-01 02:18:24 +0900

    Refactor : LocalDate, LocalDateTime 응답 변환 리팩토링
    - 가장 기본적인 패턴이 'yyyy-MM-dd'이므로 유틸 클래스에서 기본적으로 사용할 수 있도록 변경
    - ApplicationLetterServiceImpl : 모든 메서드가 `@Transactional(readOnly = true)`인 점에서 클래스 레벨 어노테이션을 변경하여 중복 제거

--- src/main/java/com/miracle/userservice/util/DateFormatUtil.java
+++ src/main/java/com/miracle/userservice/util/DateFormatUtil.java
@@ -6,8 +6,18 @@ import java.time.format.DateTimeFormatter;
 
 public abstract class DateFormatUtil {
 
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+
+    public static String dateToString(LocalDateTime localDateTime) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT));
+    }
+
     public static String dateToString(LocalDateTime localDateTime, String format) {
         return localDateTime.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static String dateToString(LocalDate localDate) {
+        return localDate.format(DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT));
     }
 
     public static String dateToString(LocalDate localDate, String format) {
