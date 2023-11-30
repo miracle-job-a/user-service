b5b84cddd3963613c650799f301b420b72147b37
hazzokko <hazzokko@gmail.com>
2023-11-30 12:24:01 +0900

    Fix : 자기소개서 등록 API 관련 수정
    1. Repository, Dto : 불필요한 코드 삭제(메서드, 변수)
    2. Controller, Service : userId 매개변수 추가

--- src/main/java/com/miracle/userservice/service/CoverLetterService.java
+++ src/main/java/com/miracle/userservice/service/CoverLetterService.java
@@ -8,6 +8,7 @@ import com.miracle.userservice.exception.NoSuchCoverLetterException;
 import java.util.List;
 
 public interface CoverLetterService {
+
     /**
      * 유저의 자기소개서 목록 조회
      *
@@ -21,44 +22,45 @@ public interface CoverLetterService {
     /**
      * 유저의 자기소개서 내용 조회
      *
-     * @param id 자기소개서 ID
+     * @param coverLetterId 자기소개서 ID
      * @return 자기소개서 상세 데이터 {@code CoverLetterResponseDto}를 반환
      * @throws NullPointerException       If {@code id} is null
      * @throws NoSuchCoverLetterException If coverLetter doesn't exist
      * @author hazzokko
      */
-    CoverLetterResponseDto getCoverLetterDetail(Long id);
+    CoverLetterResponseDto getCoverLetterDetail(Long coverLetterId);
 
     /**
      * 유저의 자기소개서 등록
      *
+     * @param userId 유저 ID
      * @param dto 자기소개서 정보
      * @return true
      * @throws NullPointerException If {@code dto} is null
      * @author hazzokko
      */
-    boolean postCoverLetter(CoverLetterPostRequestDto dto);
+    boolean postCoverLetter(Long userId, CoverLetterPostRequestDto dto);
 
     /**
      * 유저의 자기소개서 수정
      *
-     * @param id  자기소개서 ID
+     * @param coverLetterId  자기소개서 ID
      * @param dto 자기소개서 정보
      * @return true
      * @throws NullPointerException       If {@code dto} is null
      * @throws NoSuchCoverLetterException If coverLetter doesn't exist
      * @author hazzokko
      */
-    boolean updateCoverLetter(Long id, CoverLetterPostRequestDto dto);
+    boolean updateCoverLetter(Long coverLetterId, CoverLetterPostRequestDto dto);
 
     /**
      * 유저의 자기소개서 삭제
      *
-     * @param id 자기소개서 ID
+     * @param coverLetterId 자기소개서 ID
      * @return true
      * @throws NullPointerException       If {@code id} is null
      * @throws NoSuchCoverLetterException If coverLetter doesn't exist
      * @author hazzokko
      */
-    boolean deleteCoverLetter(Long id);
+    boolean deleteCoverLetter(Long coverLetterId);
 }
