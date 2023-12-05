554c3eec7027a3af64c371d4579ce201634b75d4
hazzokko <hazzokko@gmail.com>
2023-12-05 17:14:31 +0900

    Feat : 면접 정보 중복 저장 예외 추가

---
+++ src/main/java/com/miracle/userservice/exception/DuplicateInterviewException.java
@@ -0,0 +1,8 @@
+package com.miracle.userservice.exception;
+
+public class DuplicateInterviewException extends MiracleException {
+
+    public DuplicateInterviewException(String code, String msg) {
+        super(code, msg);
+    }
+}
--- src/main/java/com/miracle/userservice/repository/InterviewRepository.java
+++ src/main/java/com/miracle/userservice/repository/InterviewRepository.java
@@ -3,5 +3,8 @@ package com.miracle.userservice.repository;
 import com.miracle.userservice.entity.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
+    Optional<Interview> findByApplicationLetterId(Long applicationLetterId);
 }
